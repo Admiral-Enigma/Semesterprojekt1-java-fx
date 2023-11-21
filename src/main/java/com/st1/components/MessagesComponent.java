e8899472847c52720f2dc42426ab7b9aeb19c84e
Alexander <lord-squirrel@protonmail.com>
2023-11-21 10:42:34 +0100

    WIP message component

---
+++ src/main/java/com/st1/components/MessagesComponent.java
@@ -0,0 +1,10 @@
+package com.st1.components;
+
+import java.io.PrintStream;
+
+public class MessagesComponent extends SimpleComponent {
+    public MessagesComponent() {
+        super("components/messages-component.fxml");
+
+    }
+}
