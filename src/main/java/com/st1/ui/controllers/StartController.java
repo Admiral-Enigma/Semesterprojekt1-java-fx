e89c5ff2d0d44d204613c8809705813d5d8368e1
Admiral-Enigma <lord-squirrel@protonmail.com>
2023-11-24 17:21:46 +0100

    basic item drop

--- src/main/java/com/st1/ui/controllers/StartController.java
+++ src/main/java/com/st1/ui/controllers/StartController.java
@@ -1,12 +1,34 @@
 package com.st1.ui.controllers;
 
 import com.st1.Game;
+import com.st1.inventory.items.Hakke;
+import com.st1.inventory.items.McFeast;
+import com.st1.ui.components.ItemDrop;
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.VBox;
 
-public class StartController extends ViewController{
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class StartController extends ViewController implements Initializable {
+    public VBox mcfeastItem;
+    public VBox hakke;
+
     public void siuu(ActionEvent actionEvent) {
         Game.context.transition("road");
 
         Game.textPrinter.printText("SIUUUUU");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        McFeast mcFeast = new McFeast();
+        mcFeast.setQuantity(200);
+
+        Hakke pickaxe = new Hakke();
+
+        new ItemDrop(mcfeastItem, mcFeast);
+        new ItemDrop(hakke, pickaxe);
     }
 }
