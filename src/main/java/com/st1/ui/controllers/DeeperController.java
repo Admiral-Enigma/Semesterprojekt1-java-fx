fd241c27c55948844ba4f77914b25911b367f0c2
Andreas Bønløkke <anboe23@student.sdu.dk>
2023-11-30 16:07:58 +0100

    deep med hakke

--- src/main/java/com/st1/ui/controllers/DeeperController.java
+++ src/main/java/com/st1/ui/controllers/DeeperController.java
@@ -1,15 +1,29 @@
 package com.st1.ui.controllers;
 
 import com.st1.Game;
+import com.st1.inventory.items.Hakke;
+import com.st1.ui.components.ItemDrop;
 import com.st1.core.Direction;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.VBox;
 
-public class DeeperController extends ViewController {
-    public Label title;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DeeperController extends ViewController implements Initializable {
+    public VBox hakke;
+
 
     public void gotoSurface(ActionEvent actionEvent) {
         Game.context.transition(Direction.thorium_mine);
 
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Hakke pickaxe = new Hakke();
+
+        new ItemDrop(hakke, pickaxe);
     }
 }
