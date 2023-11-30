75f6e8accd6659142f8f708def9173ec85b4e379
Andreas Bønløkke <anboe23@student.sdu.dk>
2023-11-30 19:24:04 +0100

    fjenet hakke fra StartController

--- src/main/java/com/st1/ui/controllers/StartController.java
+++ src/main/java/com/st1/ui/controllers/StartController.java
@@ -1,20 +1,17 @@
 package com.st1.ui.controllers;
 
 import com.st1.Game;
-import com.st1.inventory.items.Hakke;
 import com.st1.inventory.items.McFeast;
 import com.st1.ui.components.ItemDrop;
 import com.st1.core.Direction;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.VBox;
-
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class StartController extends ViewController implements Initializable {
     public VBox mcfeastItem;
-    public VBox hakke;
 
     public void siuu(ActionEvent actionEvent) {
         Game.context.transition(Direction.power_plant_outside);
@@ -27,10 +24,8 @@ public class StartController extends ViewController implements Initializable {
         McFeast mcFeast = new McFeast();
         mcFeast.setQuantity(200);
 
-        Hakke pickaxe = new Hakke();
-
         new ItemDrop(mcfeastItem, mcFeast);
-        new ItemDrop(hakke, pickaxe);
+
     }
 }
 
