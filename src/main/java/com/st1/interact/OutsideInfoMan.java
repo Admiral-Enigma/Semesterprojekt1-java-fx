cb4865211084e82a4c989bf1c7992969719643fa
esrav20 <esrav20@student.sdu.dk>
2023-12-07 10:38:37 +0100

    changes to npc dialogue and placement

--- src/main/java/com/st1/interact/OutsideInfoMan.java
+++ src/main/java/com/st1/interact/OutsideInfoMan.java
@@ -8,15 +8,15 @@ public class OutsideInfoMan extends BaseNpc {
     @Override
     public String firstSightingMessage() {
         return
-                "Hej (name). Jeg må igen, undertrykke vores taknemmelighed for at du vil hjælpe os,  \n"
+                          "Hej. Jeg må igen udtrykke vores taknemmelighed for at du vil hjælpe os,  \n"
                         + "Vi i Yurigistan har en gammel nedlagt kulkraftsreaktor.\n"
                         + "Hvis du forsætter mod lobby, vil du finde to rum. \n"
                         + "I de rum kan du placere de ressourcer vi skal bruge for at omdanne reaktoren til en Small Modular Reactor. \n"
                         + "Denne reaktor vil give nogenlunde samme energi til nærområdet, hvilket sikrer at vi ikke bruger for meget eller for lidt energi. \n"
                         + "Reaktoren vil generere omkring 300 Megawatt, men producere strøm hele tiden. I modsætning til kulkraftsreaktor, som har en gennemsnitlig oppetid på 50% \n"
-                        + "Men som måske genererer 600 Megawatt. Dette gør reaktoren meget mere bæredygtig."
+                        + "Men som måske genererer 600 Megawatt.  Dette gør reaktoren meget mere bæredygtig."
                         + "Bevæg dig rundt omkring i verdenen, for at finde de ressourcer du skal bruge. \n"
-                        + "Du kan gå ind i kraftværkets reaktorrum, for at se hvilke ressourcer, du skal bruge:";
+                        + "Du kan gå ind i kraftværkets reaktorrum, for at se hvilke ressourcer, du skal bruge.";
 
     }
 
